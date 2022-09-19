784713e154c8ca05203df300bb81095f8febafaa
junaiyet <junaiyetmia470@gmail.com>
2022-09-20 00:15:18 +0600

    preventDefault

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Portfolio from "./component/Portfolio/Portfolio";
 import Testimonial from "./component/Testimonial/Testimonial";
 import Blog from "./component/Blog/Blog";
 import Footer from "./component/Footer/Footer";
-
-
 import {useState,useEffect}  from 'react'
+//Bootstrap
+import {Button,Container,Form } from 'react-bootstrap';
 
 
 function App() {
@@ -24,15 +24,47 @@ function App() {
   // useEffect(()=>{
   //   console.log("ami use homu na")
   // },[number])
-  let [show,setShow] = useState(false);
-  let handleShow=()=>{
-    setShow(!show)
+  // let [show,setShow] = useState(false);
+  // let handleShow=()=>{
+  //   setShow(!show)
     
+  // }
+  let [batchname,setBatchName] = useState("")
+  let [classname,setClassName] = useState("")
+  let [classtopic,setClassTopic] = useState("")
+  // let [attendace,setAttendace] = useState(true)
+  let handelBatchName=(e)=>{
+    setBatchName(e.target.value)
   }
-
+  let handelClassName=(e)=>{
+    setClassName(e.target.value)
+  }
+  let handelClassTopic=(e)=>{
+    setClassTopic(e.target.value)
+  }
+  let handelAttendace=(name)=>{
+    // if(attendace){
+    //   setAttendace(false);
+    //   console.log(attendace,name);
+    // }else{
+    //   setAttendace(true);
+    //   console.log(attendace,name);
+    // }
+    console.log(name)
+  };
+  let handleSubmit =(e)=>{
+    e.preventDefault();
+    let info ={
+      batchname : batchname,
+      classname : classname,
+      classtopic : classtopic,
+      // present : [],
+    }
+    console.log(info)
+  }
   return (
-    <>
-    {/* <Navber/>
+  //  <>
+    /* <Navber/>
     <Banner/>
     <About/>
     <Skill/>
@@ -40,22 +72,57 @@ function App() {
     <Portfolio/>
     <Testimonial/>
     <Blog/>
-    <Footer/> */}
-    { show ? <div style={box}></div> :<div style={box}>lorem</div>}
+    <Footer/> */
+    /* { show ? <div style={box}></div> :<div style={box}>lorem</div>} */
    
-    {/* <h1>{number}</h1>
-    <button onClick={handkeClick}>Click</button> */}
-    <button onClick={handleShow}>Click</button>
+   /* <h1>{number}</h1>
+    <button onClick={handkeClick}>Click</button> */
+    /* <button onClick={handleShow}>Click</button> */
+    // </>
+    <>
+      <Container>
+       <Form className="mt-5">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Batch Name</Form.Label>
+        <Form.Control onChange={handelBatchName} type="text"  />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Class Number</Form.Label>
+        <Form.Control onChange={handelClassName} type="text"  />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Class Topic </Form.Label>
+        <Form.Control onChange={handelClassTopic} type="text" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Shawon">
+        <Form.Check onChange={()=>handelAttendace("Shawon")}  type="checkbox" label="Shawon" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Junaiyet">
+        <Form.Check onChange={()=>handelAttendace("Junaiyet")} type="checkbox" label="Junaiyet" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Foysal">
+        <Form.Check onChange={()=>handelAttendace("Foysal")}  type="checkbox" label="Foysal" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Tahmid">
+        <Form.Check  onChange={()=>handelAttendace("Tahmid")}   type="checkbox" label="Tahmid" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="Imran">
+        <Form.Check onChange={()=>handelAttendace("Imran")} type="checkbox" label="Imran" />
+      </Form.Group>
+      <Button  onClick={handleSubmit} variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      </Container>
     </>
-    
 
   );
 }
-let box={
-  background:"red",
-  width:"300px",
-  height:"300px",
-  marginTop:"20px",
-}
-//24minit part 2
+// let box={
+//   background:"red",
+//   width:"300px",
+//   height:"300px",
+//   marginTop:"20px",
+// }
+//16+minit part 2
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGiMlejvKT878TYxA1iGUol4PCC4QX2BM",
+  authDomain: "attendace-69c0a.firebaseapp.com",
+  projectId: "attendace-69c0a",
+  storageBucket: "attendace-69c0a.appspot.com",
+  messagingSenderId: "302331948475",
+  appId: "1:302331948475:web:ff923712465fd006e8e89e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
